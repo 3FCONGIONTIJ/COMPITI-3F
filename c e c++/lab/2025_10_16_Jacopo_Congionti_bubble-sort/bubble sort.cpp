3ef60061613bd37fd8514f967be7095b3c9f1978
AlessandroOnorati <onorati.alessandro.stud@itisgalileiroma.it>
2025-11-20 09:34:46 +0100

    sistemato bubble sort

--- c e c++/lab/2025_10_16_Jacopo_Congionti_bubble-sort/bubble sort.cpp
+++ c e c++/lab/2025_10_16_Jacopo_Congionti_bubble-sort/bubble sort.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main(){
 	int scambio;
-	int arr[10];
+	int arr[11];
 	cout<<"inserire i numeri dell'array";
-	for(int i=0;i<10;i++){
+	for(int i=0;i<11;i++){
 		cin>>arr[i];
 	}
-	for (int i=0;i<10;i++){
-		for (int a=0;a<10-1;a++){
+	for (int i=0;i<11;i++){
+		for (int a=0;a<11-1;a++){
 			if(arr[a]>arr[a+1]){
 			    scambio=arr[a];
 				arr[a]=arr[a+1];
@@ -17,14 +17,14 @@ int main(){
 		}
 	}
 	cout<<"numeri ordinati in ordine crescente:\n";
-	for (int i=0;i<10;i++){
+	for (int i=0;i<11;i++){
 		cout<<arr[i];
-		if (i<9){
+		if (i<10){
 			cout<<",";
 		}
 	}
 	cout<<"numero maggiore\n";
-	cout<<arr[9];
+	cout<<arr[10];
 	cout<<"numero minore\n";
 	cout<<arr[0];
 }
